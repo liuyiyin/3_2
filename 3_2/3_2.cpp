c376d27f79fa8c29a3309448a739e30efb676414
lenovo <lenovo@LAPTOP-P8K9KTT5>
2020-03-13 14:33:38 +0800

    3_2

--- 3_2/3_2.cpp
+++ 3_2/3_2.cpp
@@ -2,10 +2,20 @@
 //
 
 #include "stdafx.h"
-
+#include <opencv2/opencv.hpp>
+using namespace cv;
 
 int main()
 {
+	cv::Mat srcMat = imread("D:\\1.jpg",0);
+	cv::Mat dajinMat;
+	cv::Mat quyuMat;
+	threshold(srcMat, dajinMat, 100, 255, THRESH_BINARY);
+	adaptiveThreshold(srcMat,quyuMat , 255, ADAPTIVE_THRESH_GAUSSIAN_C, THRESH_BINARY_INV, 15, 10);
+
+	imshow("大津法二值化", dajinMat);
+	imshow("区域自适应二值化", quyuMat);
+	waitKey(0);
     return 0;
 }
 
